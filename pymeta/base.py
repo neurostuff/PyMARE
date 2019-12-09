e66c925946ad095e36c2115a8994d2a4d832d8a7
Tal Yarkoni <tyarkoni@gmail.com>
2019-12-09 12:18:16 -0600

    add Dataset class

--- pymeta/base.py
+++ pymeta/base.py
@@ -1,20 +1,36 @@
 import numpy as np
 import scipy as sp
 
-from .estimators import fixed_effects, dersimonian_laird
+from .estimators import WeightedLeastSquares, DerSimonianLaird
 from .likelihoods import meta_regression_ml_nll, meta_regression_reml_nll
+
+
+class Dataset:
+
+    def __init__(self, y, v, X=None, add_intercept=True):
+        self.y = y
+        self.v = v
+        self.add_intercept = add_intercept
+        self.X = self._get_X(X)
+
+        # Precompute commonly used quantities
+        self.k = len(y)
+        self.p = self.X.shape[1]
+
+    def _get_X(self, X):
+        if self.add_intercept:
+            intercept = np.ones((len(y), 1))
+            X = intercept if X is None else np.c_[intercept, X]
+        if X is None:
+            raise ValueError("No fixed predictors found. If no X matrix is "
+                            "provided, add_intercept must be True!")
+        return X
 
 
 def meta_regression(y, v, X=None, method='ML', beta=None, tau2=None,
                     add_intercept=True, **optim_kwargs):
 
-    if add_intercept:
-        intercept = np.ones((len(y), 1))
-        X = intercept if X is None else np.c_[intercept, X]
-
-    if X is None:
-        raise ValueError("No fixed predictors found. If no X matrix is "
-                         "provided, add_intercept must be True!")
+    dataset = Dataset(y, v, X, add_intercept)
 
     method = method.lower()
 
@@ -23,7 +39,7 @@ def meta_regression(y, v, X=None, method='ML', beta=None, tau2=None,
 
         # use D-L estimate for initial values
         if tau2 is None or beta is None:
-            _beta_dl, _tau2_dl = dersimonian_laird(y, v, X)
+            _beta_dl, _tau2_dl = DerSimonianLaird().fit(dataset)
             if beta is None:
                 beta = _beta_dl
             if tau2 is None:
@@ -36,17 +52,17 @@ def meta_regression(y, v, X=None, method='ML', beta=None, tau2=None,
             'reml': meta_regression_reml_nll
         }[method]
 
-        sigma = np.diag(v)
-        args = (y, v, X, sigma)
-        res = sp.optimize.minimize(ll_func, theta_init, args, **optim_kwargs).x
+        res = sp.optimize.minimize(ll_func, theta_init, dataset,
+                                   **optim_kwargs).x
         beta, tau = res[:-1], float(res[-1])
         tau = np.max([tau, 0])
 
     # Analytical estimation methods
     else:
-        estimator = {
-            'dl': dersimonian_laird
+        Estimator = {
+            'dl': DerSimonianLaird
         }[method]
-        beta, tau = estimator(y, v, X)
+        est = Estimator()
+        beta, tau = est.fit(dataset)
 
     return beta, tau
