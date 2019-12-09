5780d2463b2497fb18ef0140d48a92ecaf832616
Tal Yarkoni <tyarkoni@gmail.com>
2019-12-09 12:13:32 -0600

    refactor estimators into classes

--- pymeta/estimators.py
+++ pymeta/estimators.py
@@ -1,32 +1,46 @@
+from abc import ABCMeta, abstractmethod
+
 import numpy as np
 
 
-def fixed_effects_meta_analysis(y, v):
-    """ Standard inverse variance-weighted fixed-effects meta-analysis. """
-    w = 1./v
-    return y.dot(w) / w.sum()
+class Estimator(metaclass=ABCMeta):
+
+    @abstractmethod
+    def fit(self, dataset):
+        pass
+
+    def set_params(self, **params):
+        for (key, value) in params.items():
+            setattr(self, key, value)
 
 
-def weighted_least_squares(y, v, X, tau2=0):
+class WeightedLeastSquares(Estimator):
     """ Weighted least-squares estimation of fixed effects. """
-    w = 1. / (v + tau2)
-    return (np.linalg.pinv((X.T * w).dot(X)).dot(X.T) * w).dot(y)
+
+    def __init__(self, tau2=0):
+        self.tau2 = tau2
+
+    def fit(self, dataset):
+        v, tau2, X, y = dataset.v, self.tau2, dataset.X, dataset.y
+        w = 1. / (v + tau2)
+        return (np.linalg.pinv((X.T * w).dot(X)).dot(X.T) * w).dot(y)
 
 
-def dersimonian_laird(y, v, X):
-    """ DerSimonian-Laird random effects estimator. """
-    k = len(y)
-    p = X.shape[1]
-    # WLS estimate of beta with tau^2 = 0
-    beta_wls = weighted_least_squares(y, v, X)
-    # Cochrane's Q
-    w = 1. / v
-    w_sum = w.sum()
-    Q = (w * (y - X.dot(beta_wls))**2).sum()
-    # D-L estimate of tau^2
-    precision = np.linalg.pinv((X.T * w).dot(X))
-    A = w_sum - np.trace((precision.dot(X.T) * w**2).dot(X))
-    tau_dl = np.max([0., (Q - k + p) / A])
-    # Re-estimate beta with tau^2 estimate
-    beta_dl = weighted_least_squares(y, v, X, tau2=tau_dl)
-    return beta_dl, tau_dl
+class DersimonianLaird(Estimator):
+    """ DerSimonian-Laird meta-regression estimator. """
+
+    def fit(self, dataset):
+        y, X, v, k, p = dataset.y, dataset.X, dataset.v, dataset.k, dataset.p
+        # WLS estimate of beta with tau^2 = 0
+        beta_wls = WeightedLeastSquares(0).fit(dataset)
+        # Cochrane's Q
+        w = 1. / v
+        w_sum = w.sum()
+        Q = (w * (y - X.dot(beta_wls))**2).sum()
+        # D-L estimate of tau^2
+        precision = np.linalg.pinv((X.T * w).dot(X))
+        A = w_sum - np.trace((precision.dot(X.T) * w**2).dot(X))
+        tau_dl = np.max([0., (Q - k + p) / A])
+        # Re-estimate beta with tau^2 estimate
+        beta_dl = WeightedLeastSquares(tau_dl).fit(dataset)
+        return beta_dl, tau_dl
