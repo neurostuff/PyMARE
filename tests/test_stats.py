d371c1d530aa0750c07707a6d69872c410fd172f
Tal Yarkoni <tyarkoni@gmail.com>
2019-12-17 16:08:08 -0600

    add stats tests

---
+++ tests/test_stats.py
@@ -0,0 +1,15 @@
+
+from pymare.stats import q_gen, q_profile
+
+
+def test_q_gen(vars_with_intercept):
+    result = q_gen(*vars_with_intercept, 8)
+    assert round(result, 4) == 8.0161
+
+
+def test_q_profile(vars_with_intercept):
+    bounds = q_profile(*vars_with_intercept, 0.05)
+    assert set(bounds.keys()) == {'ci_l', 'ci_u'}
+    print(bounds)
+    assert round(bounds['ci_l'], 4) == 3.8076
+    assert round(bounds['ci_u'], 4) == 59.616
