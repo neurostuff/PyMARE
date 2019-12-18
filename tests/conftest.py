ecc2ba67acd896940d8f906b803ad0c44b0606e1
Tal Yarkoni <tyarkoni@gmail.com>
2019-12-18 12:59:05 -0600

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,25 @@
 import pytest
 import numpy as np
 
+from pymare import Dataset
+
 
 @pytest.fixture(scope='package')
 def variables():
-    y = np.array([-1, 0.5, 0.5, 0.5, 1, 1, 2, 10])
-    v = np.array([1, 1, 2.4, 0.5, 1, 1, 1.2, 1.5])
+    y = np.array([[-1, 0.5, 0.5, 0.5, 1, 1, 2, 10]]).T
+    v = np.array([[1, 1, 2.4, 0.5, 1, 1, 1.2, 1.5]]).T
     X = np.array([1, 1, 2, 2, 4, 4, 2.8, 2.8])
     return (y, v, X)
 
 
 @pytest.fixture(scope='package')
+def dataset(variables):
+    return Dataset(*variables, names=['my_covariate'])
+
+
+@pytest.fixture(scope='package')
 def vars_with_intercept():
-    y = np.array([-1, 0.5, 0.5, 0.5, 1, 1, 2, 10])
-    v = np.array([1, 1, 2.4, 0.5, 1, 1, 1.2, 1.5])
+    y = np.array([[-1, 0.5, 0.5, 0.5, 1, 1, 2, 10]]).T
+    v = np.array([[1, 1, 2.4, 0.5, 1, 1, 1.2, 1.5]]).T
     X = np.array([np.ones(8), [1, 1, 2, 2, 4, 4, 2.8, 2.8]]).T
     return (y, v, X)
--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -4,51 +4,50 @@ from pymare.estimators import (WeightedLeastSquares, DerSimonianLaird,
                          LikelihoodBased, StanMetaRegression)
 
 
-def test_weighted_least_squares_estimator(vars_with_intercept):
+def test_weighted_least_squares_estimator(dataset):
     # ground truth values are from metafor package in R
-    results = WeightedLeastSquares()._fit(*vars_with_intercept)
-    beta, tau2 = results['beta'], results['tau2']
+    results = WeightedLeastSquares().fit(dataset)
+    beta, tau2 = results['beta']['est'], results['tau2']['est']
     assert np.allclose(beta, [-0.2725, 0.6935], atol=1e-4)
     assert tau2 == 0.
 
     # With non-zero tau^2
-    results = WeightedLeastSquares(8.)._fit(*vars_with_intercept)
-    beta, tau2 = results['beta'], results['tau2']
+    results = WeightedLeastSquares(8.).fit(dataset)
+    beta, tau2 = results['beta']['est'], results['tau2']['est']
     assert np.allclose(beta, [-0.1071, 0.7657], atol=1e-4)
     assert tau2 == 8.
 
 
-def test_dersimonian_laird_estimator(vars_with_intercept):
+def test_dersimonian_laird_estimator(dataset):
     # ground truth values are from metafor package in R
-    results = DerSimonianLaird()._fit(*vars_with_intercept)
-    beta, tau2 = results['beta'], results['tau2']
+    results = DerSimonianLaird().fit(dataset)
+    beta, tau2 = results['beta']['est'], results['tau2']['est']
     assert np.allclose(beta, [-0.1070, 0.7664], atol=1e-4)
     assert np.allclose(tau2, 8.3627, atol=1e-4)
 
 
-def test_maximum_likelihood_estimator(vars_with_intercept):
+def test_maximum_likelihood_estimator(dataset):
     # ground truth values are from metafor package in R
-    results = LikelihoodBased(method='ML')._fit(*vars_with_intercept)
-    beta, tau2 = results['beta'], results['tau2']
+    results = LikelihoodBased(method='ML').fit(dataset)
+    beta, tau2 = results['beta']['est'], results['tau2']['est']
     assert np.allclose(beta, [-0.1072, 0.7653], atol=1e-4)
     assert np.allclose(tau2, 7.7649, atol=1e-4)
 
 
-def test_restricted_maximum_likelihood_estimator(vars_with_intercept):
+def test_restricted_maximum_likelihood_estimator(dataset):
     # ground truth values are from metafor package in R
-    results = LikelihoodBased(method='REML')._fit(*vars_with_intercept)
-    beta, tau2 = results['beta'], results['tau2']
+    results = LikelihoodBased(method='REML').fit(dataset)
+    beta, tau2 = results['beta']['est'], results['tau2']['est']
     assert np.allclose(beta, [-0.1066, 0.7700], atol=1e-4)
     assert np.allclose(tau2, 10.9499, atol=1e-4)
 
 
-def test_stan_estimator(vars_with_intercept):
+def test_stan_estimator(dataset):
     # no ground truth here, so we use sanity checks and rough bounnds
-    results = StanMetaRegression(iter=1200)._fit(*vars_with_intercept)
-    assert 'StanFit4Model' == results.__class__.__name__
+    results = StanMetaRegression(iter=2500).fit(dataset)
+    assert 'BayesianMetaRegressionResults' == results.__class__.__name__
     summary = results.summary(['beta', 'tau2'])
-    beta = summary['summary'][:2, 0]
-    tau2 = summary['summary'][2, 0]
-    assert -0.4 < beta[0] < 0.1
-    assert 0.6 < beta[1] < 0.9
+    beta1, beta2, tau2 = summary['mean'].values[:3]
+    assert -0.5 < beta1 < 0.1
+    assert 0.6 < beta2 < 0.9
     assert 2 < tau2 < 6
