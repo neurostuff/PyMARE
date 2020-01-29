6818bc5c22fe4c43bd3eaccbe9abd2a21a1788cd
Tal Yarkoni <tyarkoni@gmail.com>
2020-01-29 12:41:40 -0600

    add docstrings

--- pymare/effectsizes/base.py
+++ pymare/effectsizes/base.py
@@ -1,3 +1,5 @@
+"""Tools for effect size computation/conversion."""
+
 from typing import Iterable
 import warnings
 import inspect
@@ -8,12 +10,14 @@ from sympy import sympify, lambdify, nonlinsolve, Symbol
 from pymare import Dataset
 
 
+# Equations that cover conversion between basic quantities.
 BASE_EQUATIONS = [
     'sd - sqrt(v)',
     'sem - sd / sqrt(n)',
     't - y / sem'
 ]
 
+# Standardized effect size metrics.
 EFFECT_SIZE_EQUATIONS = [
     'd - y / sd',
     'g - y * j / sd',
@@ -22,6 +26,32 @@ EFFECT_SIZE_EQUATIONS = [
 
 
 def get_system(mode='base', target=None, known_vars=None):
+    """Returns a system of SymPy equations that meet specified criteria.
+    
+    Args:
+        mode (str, optional): Describes the set of equations to start from.
+            Options include:
+                * 'base': Equations to compute basic quantities from common
+                  inputs like variance, point estimate, etc.
+                * 'onesample': All effect size metrics that apply to the
+                  one-sample case.
+                * 'paired': All effect size metrics that apply to the paired
+                  samples case.
+                * 'twosample': All effect size metrics that apply to the
+                  independent samples case.
+        target (str, optional): The name of the target output metric. Can be
+            any quantity that occurs at least once in an equation (e.g., 't',
+            'g', 'sem', etc.). If provided, the system will contain only a
+            single equation in the event that one is sufficient to produce the
+            desired target. Defaults to None, in which case all equations in
+            the set specified by the `mode` will be returned.
+        known_vars ([str], optional): An iterable of strings giving the names
+            of any known variables. These will be used to screen equations in
+            the event that `target` is provided. Defaults to None.
+    
+    Returns:
+        [sympy.core.expr.Expr]: A list of sympy expressions.
+    """
     # Build list of candidate expressions based on the mode
     if mode == 'base':
         exprs = BASE_EQUATIONS
@@ -44,9 +74,28 @@ def get_system(mode='base', target=None, known_vars=None):
     return exprs
 
 
-def solve_system(system, known_vars):
+def solve_system(system, known_vars=None):
+    """Solve and evaluate a system of SymPy equations given known inputs.
+    
+    Args:
+        system ([sympy.core.expr.Expr]): A list of SymPy expressions defining
+            the system to solve.
+        known_vars (dict, optional): A dictionary of known variables to use
+            when evaluating the solution. Keys are the names of parameters
+            (e.g., 'sem', 't'), values are numerical data types (including
+            numpy arrays).
+    
+    Returns:
+        A dictionary of newly computed values, where the keys are parameter
+        names and the values are numerical data types.
 
+    Notes:
+        The returned dictionary contains only keys that were not passed in as
+        input (i.e., already known variables will be ignored).
+    """
     system = system.copy()
+
+    known_vars = known_vars or {}
 
     # Get base system of equations and construct symbol dict
     symbols = set().union(*[eq.free_symbols for eq in system])
@@ -86,32 +135,30 @@ def solve_system(system, known_vars):
 
 
 class EffectSizeConverter:
-
-    def __init__(
-            self,
-            dataset: Dataset=None,
-            y: Iterable[float]=None,
-            v: Iterable[float]=None,
-            n: Iterable[float]=None,
-            t: Iterable[float]=None,
-            d: Iterable[float]=None,
-            y2: Iterable[float]=None,
-            v2: Iterable[float]=None,
-            n2: Iterable[float]=None, 
-            paired: bool=True):
-        """[summary]
-        
-        Args:
-            dataset (Dataset, optional): [description]. Defaults to None.
-            y (Iterable[float], optional): [description]. Defaults to None.
-            v (Iterable[float], optional): [description]. Defaults to None.
-            n (Iterable[float], optional): [description]. Defaults to None.
-            t (Iterable[float], optional): [description]. Defaults to None.
-            y2 (Iterable[float], optional): [description]. Defaults to None.
-            v2 (Iterable[float], optional): [description]. Defaults to None.
-            n2 (Iterable[float], optional): [description]. Defaults to None.
-            paired (bool, optional): [description]. Defaults to True.
-        """
+    """Converts between effect size metrics and dependent quantities.
+    
+    Args:
+        dataset (Dataset, optional): Optional PyMARE dataset to extract input
+            quantities from. Other arguments will take precedent over values
+            found in the Dataset. Defaults to None.
+        y ((float, iterable), optional): Point estimate(s) (e.g., means).
+            Defaults to None.
+        v ((float, iterable), optional): Variance(s). Defaults to None.
+        n ((float, iterable), optional): Sample size(s). Defaults to None.
+        t ((float, iterable), optional): t-statistic(s). Defaults to None.
+        y2 ((float, iterable), optional): Second set of of point estimates, in
+            the paired or two-sample case. Defaults to None.
+        v2 ((float, iterable), optional): Second set of variances, in the
+            paired or two-sample case. Defaults to None.
+        n2 ((float, iterable), optional): [description]. Second set of sample
+            sizes, in the paired or two-sample case. Defaults to None.
+        paired (bool, optional): If one of y2, v2, or n2 is defined, indicates
+            whether the inputs reflect a paired (True) or independent-samples
+            (False) comparison. Defaults to True. Ignored if only one set of
+            values is provided.
+    """
+    def __init__(self, dataset=None, y=None, v=None, n=None, t=None, d=None,
+                 y2=None, v2=None, n2=None, paired=True):
         # If two ys but only one sample size are provided and the test is
         # paired, assume the same sample size in both conditions
         if (y is not None and y2 is not None
@@ -166,7 +213,21 @@ class EffectSizeConverter:
             return partial(self.to, stat=stat)
 
     def to(self, stat):
+        """Compute and return values for the specified statistic, if possible.
 
+        Args:
+            stat (str): The name of the statistic to compute (e.g., 'd', 'g').
+        
+        Returns:
+            A float or ndarray containing the requested parameter values, if
+            successfully computed.
+
+        Notes:
+            All values computed via to() are internally cached. Do not try to
+            update the instance's known values directly and recompute
+            quantities; all changes in data require initialization of a new
+            instance.
+        """
         if stat in self.known_vars:
             return self.known_vars[stat]
 
