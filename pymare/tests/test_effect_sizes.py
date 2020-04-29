534c2d7c58c48faccd859df95abae158cf027dba
Tal Yarkoni <tyarkoni@gmail.com>
2020-04-29 15:59:09 -0500

    add 2d test and fix others

--- pymare/tests/test_effect_sizes.py
+++ pymare/tests/test_effect_sizes.py
@@ -31,12 +31,6 @@ def test_EffectSizeConverter_smoke_test(data):
     assert np.allclose(esc.get_d(), np.array([-0.63246, -0.140744]), atol=1e-5)
     assert np.allclose(esc.get_g(), np.array([-0.61065, -0.13707]), atol=1e-5)
 
-    esc = OneSampleEffectSizeConverter(z=data['z'])
-    assert np.allclose(esc.get_p(), np.array([0.975, 0.005]), atol=1e-3)
-
-    esc = OneSampleEffectSizeConverter(p=data['p'])
-    assert np.allclose(esc.get_z(), np.array([-1.645, 2.326]), atol=1e-3)
-
 
 def test_EffectSizeConverter_from_df(data):
     df = pd.DataFrame(data)
@@ -50,3 +44,22 @@ def test_EffectSizeConverter_to_dataset(data):
     dataset = esc.to_dataset(X=X, X_names=['dummy'])
     assert dataset.__class__.__name__ == 'Dataset'
     assert dataset.X_names == ['intercept', 'dummy']
+
+
+def test_2d_array_conversion():
+    shape = (10, 2)
+    data = {
+        'y': np.random.randint(10, size=shape),
+        'v': np.random.randint(1, 10, size=shape),
+        'n': np.ones(shape) * 40
+    }
+    esc = OneSampleEffectSizeConverter(**data)
+
+    sd = esc.get_sd()
+    assert np.array_equal(sd, np.sqrt(data['v']))
+
+    # smoke test other parameters to make sure all generated numpy funcs can
+    # handle 2d inputs.
+    for stat in ['g']:
+        result = esc.get(stat)
+        assert result.shape == shape
