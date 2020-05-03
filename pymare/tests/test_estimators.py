f522e5608f02443fd2ac7c5427822e5b9675c42f
Tal Yarkoni <tyarkoni@gmail.com>
2020-05-03 14:43:07 -0500

    update estimator tests

--- pymare/tests/test_estimators.py
+++ pymare/tests/test_estimators.py
@@ -11,14 +11,14 @@ def test_weighted_least_squares_estimator(dataset):
     # ground truth values are from metafor package in R
     est = WeightedLeastSquares().fit(dataset)
     results = est.summary()
-    beta, tau2 = results['beta']['est'], results['tau2']['est']
+    beta, tau2 = results.fe_params, results.tau2
     assert np.allclose(beta.ravel(), [-0.2725, 0.6935], atol=1e-4)
     assert tau2 == 0.
 
     # With non-zero tau^2
     est = WeightedLeastSquares(8.).fit(dataset)
     results = est.summary()
-    beta, tau2 = results['beta']['est'], results['tau2']['est']
+    beta, tau2 = results.fe_params, results.tau2
     assert np.allclose(beta.ravel(), [-0.1071, 0.7657], atol=1e-4)
     assert tau2 == 8.
 
@@ -27,14 +27,14 @@ def test_dersimonian_laird_estimator(dataset):
     # ground truth values are from metafor package in R
     est = DerSimonianLaird().fit(dataset)
     results = est.summary()
-    beta, tau2 = results['beta']['est'], results['tau2']['est']
+    beta, tau2 = results.fe_params, results.tau2
     assert np.allclose(beta.ravel(), [-0.1070, 0.7664], atol=1e-4)
     assert np.allclose(tau2, 8.3627, atol=1e-4)
 
 
 def test_2d_DL_estimator(dataset_2d):
     results = DerSimonianLaird().fit(dataset_2d).summary()
-    beta, tau2 = results['beta']['est'], results['tau2']['est']
+    beta, tau2 = results.fe_params, results.tau2
     assert beta.shape == (2, 3)
     assert tau2.shape == (3,)
 
@@ -54,14 +54,14 @@ def test_hedges_estimator(dataset):
     # algorithmic differences in the computation.
     est = Hedges().fit(dataset)
     results = est.summary()
-    beta, tau2 = results['beta']['est'], results['tau2']['est']
+    beta, tau2 = results.fe_params, results.tau2
     assert np.allclose(beta.ravel(), [-0.1066, 0.7704], atol=1e-4)
     assert np.allclose(tau2, 11.3881, atol=1e-4)
 
 
 def test_2d_hedges(dataset_2d):
     results = Hedges().fit(dataset_2d).summary()
-    beta, tau2 = results['beta']['est'], results['tau2']['est']
+    beta, tau2 = results.fe_params, results.tau2
     assert beta.shape == (2, 3)
     assert tau2.shape == (3,)
 
@@ -79,7 +79,7 @@ def test_variance_based_maximum_likelihood_estimator(dataset):
     # ground truth values are from metafor package in R
     est = VarianceBasedLikelihoodEstimator(method='ML').fit(dataset)
     results = est.summary()
-    beta, tau2 = results['beta']['est'], results['tau2']['est']
+    beta, tau2 = results.fe_params, results.tau2
     assert np.allclose(beta.ravel(), [-0.1072, 0.7653], atol=1e-4)
     assert np.allclose(tau2, 7.7649, atol=1e-4)
 
@@ -88,7 +88,7 @@ def test_variance_based_restricted_maximum_likelihood_estimator(dataset):
     # ground truth values are from metafor package in R
     est = VarianceBasedLikelihoodEstimator(method='REML').fit(dataset)
     results = est.summary()
-    beta, tau2 = results['beta']['est'], results['tau2']['est']
+    beta, tau2 = results.fe_params, results.tau2
     assert np.allclose(beta.ravel(), [-0.1066, 0.7700], atol=1e-4)
     assert np.allclose(tau2, 10.9499, atol=1e-4)
 
@@ -97,9 +97,9 @@ def test_sample_size_based_maximum_likelihood_estimator(dataset_n):
     # test values have not been verified for convergence with other packages
     est = SampleSizeBasedLikelihoodEstimator(method='ML').fit(dataset_n)
     results = est.summary()
-    beta = results['beta']['est']
-    sigma2 = results['sigma2']['est']
-    tau2 = results['tau2']['est']
+    beta = results.fe_params
+    sigma2 = results.estimator.params_['sigma2']
+    tau2 = results.tau2
     assert np.allclose(beta, [-2.0951], atol=1e-4)
     assert np.allclose(sigma2, 12.777, atol=1e-3)
     assert np.allclose(tau2, 2.8268, atol=1e-4)
@@ -109,9 +109,9 @@ def test_sample_size_based_restricted_maximum_likelihood_estimator(dataset_n):
     # test values have not been verified for convergence with other packages
     est = SampleSizeBasedLikelihoodEstimator(method='REML').fit(dataset_n)
     results = est.summary()
-    beta = results['beta']['est']
-    sigma2 = results['sigma2']['est']
-    tau2 = results['tau2']['est']
+    beta = results.fe_params
+    sigma2 = results.estimator.params_['sigma2']
+    tau2 = results.tau2
     assert np.allclose(beta, [-2.1071], atol=1e-4)
     assert np.allclose(sigma2, 13.048, atol=1e-3)
     assert np.allclose(tau2, 3.2177, atol=1e-4)
@@ -120,7 +120,7 @@ def test_sample_size_based_restricted_maximum_likelihood_estimator(dataset_n):
 def test_2d_looping(dataset_2d):
     est = VarianceBasedLikelihoodEstimator().fit(dataset_2d)
     results = est.summary()
-    beta, tau2 = results['beta']['est'], results['tau2']['est']
+    beta, tau2 = results.fe_params, results.tau2
     assert beta.shape == (2, 3)
     assert tau2.shape == (3,)
 
