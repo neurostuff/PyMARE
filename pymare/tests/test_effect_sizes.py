d9e3e91f901ca65c0578c7342b90077fe32fcd33
Tal Yarkoni <tyarkoni@gmail.com>
2020-05-05 17:12:05 -0500

    fix tests and add r conversion test

--- pymare/tests/test_effect_sizes.py
+++ pymare/tests/test_effect_sizes.py
@@ -21,19 +21,26 @@ def data():
 def test_EffectSizeConverter_smoke_test(data):
     esc = OneSampleEffectSizeConverter(m=data['m1'], sd=data['sd1'], n=data['n1'])
     assert set(esc.known_vars.keys()) == {'m', 'sd', 'n'}
-    assert esc.get_d().shape == data['m1'].shape
+    assert esc.get_sm().shape == data['m1'].shape
     assert not {'d', 'sd'} - set(esc.known_vars.keys())
 
     esc = TwoSampleEffectSizeConverter(**data)
     assert set(esc.known_vars.keys()) == set(data.keys())
     assert np.allclose(esc.get_d(), np.array([-0.63246, -0.140744]), atol=1e-5)
-    assert np.allclose(esc.get_g(), np.array([-0.61065, -0.13707]), atol=1e-5)
+    assert np.allclose(esc.get_smd(), np.array([-0.61065, -0.13707]), atol=1e-5)
+
+
+def test_esc_implicit_dtype_conversion():
+    esc = OneSampleEffectSizeConverter(m=[10, 12, 18])
+    assert 'm' in esc.known_vars
+    assert isinstance(esc.known_vars['m'], np.ndarray)
+    assert esc.known_vars['m'][1] == 12
 
 
 def test_EffectSizeConverter_from_df(data):
     df = pd.DataFrame(data)
     esc = TwoSampleEffectSizeConverter(df)
-    assert np.allclose(esc.get_g(), np.array([-0.61065, -0.13707]), atol=1e-5)
+    assert np.allclose(esc.get_smd(), np.array([-0.61065, -0.13707]), atol=1e-5)
 
 
 def test_EffectSizeConverter_to_dataset(data):
@@ -58,6 +65,24 @@ def test_2d_array_conversion():
 
     # smoke test other parameters to make sure all generated numpy funcs can
     # handle 2d inputs.
-    for stat in ['g']:
+    for stat in ['sm']:
         result = esc.get(stat)
         assert result.shape == shape
+
+
+def test_convert_r_and_n_to_rz():
+    r=[0.2, 0.16, 0.6]
+    n = (68, 165, 17)
+    esc = OneSampleEffectSizeConverter(r=r)
+    zr = esc.get_zr()
+    assert np.allclose(zr, np.arctanh(r))
+    # Needs n
+    with pytest.raises(ValueError, match="Unable to solve"):
+        esc.get_v_zr()
+    esc = OneSampleEffectSizeConverter(r=r, n=n)
+    v_zr = esc.get('V_ZR')
+    assert np.allclose(v_zr, 1 / (np.array(n) - 3))
+    ds = esc.to_dataset(measure="ZR")
+    assert np.allclose(ds.y.ravel(), zr)
+    assert np.allclose(ds.v.ravel(), v_zr)
+    assert ds.n is not None
