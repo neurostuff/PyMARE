2369b3f428078f8db947400173aa51cea32aaddc
Taylor Salo <tsalo006@fiu.edu>
2022-05-26 14:29:35 -0400

    Improve example.

--- examples/02_meta-analysis/plot_run_meta-analysis.py
+++ examples/02_meta-analysis/plot_run_meta-analysis.py
@@ -1,34 +1,33 @@
 # emacs: -*- mode: python-mode; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
 # ex: set sts=4 ts=4 sw=4 et:
 """
-.. _meta2:
+.. _meta_basics:
 
-========================
- Running a meta-analysis
-========================
+=======================
+Running a meta-analysis
+=======================
 
 PyMARE implements a range of meta-analytic estimators.
 """
 ###############################################################################
 # Start with the necessary imports
-# --------------------------------
-import numpy as np
-
-from pymare import core, estimators
+# -----------------------------------------------------------------------------
+from pymare import core, datasets, estimators
 
 ###############################################################################
-# Here we simulate a dataset
-# -----------------------------------
-n_studies = 100
-v = np.random.random((n_studies))
-y = np.random.random((n_studies))
-X = np.random.random((n_studies, 5))
-n = np.random.randint(5, 50, size=n_studies)
+# Here we load a dataset
+# -----------------------------------------------------------------------------
+# We will use the Michael 2013 dataset.
+#
+# We only want ot do a mean analysis, so we won't have any covariates except for
+# an intercept.
+data, meta = datasets.michael2013()
+data["total_sample_size"] = data["No_brain_n"] + data["Brain_n"]
+dset = core.Dataset(data=data, y="yi", v="vi", n="total_sample_size", X=None, add_intercept=True)
 
 ###############################################################################
-# Datasets can also be created from pandas DataFrames
-# ---------------------------------------------------
-dataset = core.Dataset(v=v, X=X, y=y, n=n)
-est = estimators.WeightedLeastSquares().fit_dataset(dataset)
+# Now we fit a model
+# -----------------------------------------------------------------------------
+est = estimators.WeightedLeastSquares().fit_dataset(dset)
 results = est.summary()
 print(results.to_df())
