d9476cefbeafa282cc9a55f767d3b9b1902fe442
Taylor Salo <tsalo006@fiu.edu>
2022-06-02 12:11:29 -0400

    Update plot_create_dataset.py

--- examples/01_basic_io/plot_create_dataset.py
+++ examples/01_basic_io/plot_create_dataset.py
@@ -22,10 +22,17 @@ from pymare import core
 ###############################################################################
 # Datasets can be created from arrays
 # -----------------------------------
+# The simplest way to create a dataset is to pass in arguments as numpy arrays.
+#
+# ``y`` refers to the study-level estimates, ``v`` to the variances,
+# ``X`` to any study-level regressors, and ``n`` to the sample sizes.
+y = [2, 4, 6]
 v = [100, 100, 100]
 X = [[5, 9], [2, 8], [1, 7]]
-y = [2, 4, 6]
-dataset = core.Dataset(y=y, v=v, X=X)
+
+dataset = core.Dataset(y=y, v=v, X=X, X_names=["X1", "X7"])
+
+pprint(vars(dataset))
 
 ###############################################################################
 # Datasets can also be created from pandas DataFrames
@@ -38,6 +45,28 @@ df = pd.DataFrame(
         "X7": [9, 8, 7],
     }
 )
+
 dataset = core.Dataset(v="v_alt", X=["X1", "X7"], data=df, add_intercept=False)
+
+pprint(vars(dataset))
+
+###############################################################################
+# Datasets can also contain parallel "datasets"
+# ---------------------------------------------
+y = [
+    [2, 4, 6],  # estimates for first "dataset"
+    [3, 2, 1],  # estimates for second "dataset"
+]
+v = [
+    [100, 100, 100],  # variance for first "dataset"'s estimates
+    [8, 4, 2],  # variance for second "dataset"'s estimates
+]
+X = [  # all "dataset"s must have the same regressors
+    [5, 9],
+    [2, 8],
+    [1, 7],
+]
+
+dataset = core.Dataset(y=y, v=v, X=X, X_names=["X1", "X7"])
 
 pprint(vars(dataset))
