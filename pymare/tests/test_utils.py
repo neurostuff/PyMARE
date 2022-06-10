d3f9e9b27875ad4eda59e6e8a21f8e94431d1d0c
JulioAPeraza <jpera054@fiu.edu>
2022-06-09 21:37:11 -0400

    Add test for nimare.utils._check_inputs_shape

--- pymare/tests/test_utils.py
+++ pymare/tests/test_utils.py
@@ -1,6 +1,8 @@
 """Tests for pymare.utils."""
 import os.path as op
 
+import numpy as np
+
 from pymare import utils
 
 
@@ -8,3 +10,20 @@ def test_get_resource_path():
     """Test nimare.utils.get_resource_path."""
     print(utils.get_resource_path())
     assert op.isdir(utils.get_resource_path())
+
+
+def test_check_inputs_shape():
+    """Test nimare.utils._check_inputs_shape."""
+    n_rows = 5
+    n_columns = 4
+    n_pred = 3
+    y = np.random.randint(1, 100, size=(n_rows, n_columns))
+    v = np.random.randint(1, 100, size=(n_rows, n_columns))
+    n = np.random.randint(1, 100, size=(n_rows, n_columns))
+    X = np.random.randint(1, 100, size=(n_rows, n_pred))
+    X_names = [f"X{x}" for x in range(n_pred)]
+
+    utils._check_inputs_shape(y, X, "y", "X", row=True)
+    utils._check_inputs_shape(y, v, "y", "v", row=True, column=True)
+    utils._check_inputs_shape(y, n, "y", "n", row=True, column=True)
+    utils._check_inputs_shape(X, np.array(X_names)[None, :], "X", "X_names", column=True)
