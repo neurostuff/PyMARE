52c5374e332ad3d1761bf9111d7d1f6e25a742dd
JulioAPeraza <jpera054@fiu.edu>
2023-01-25 11:51:22 -0500

    Add deprecation warnings for Python 3.6 and 3.7

--- pymare/__init__.py
+++ pymare/__init__.py
@@ -1,4 +1,7 @@
 """PyMARE: Python Meta-Analysis & Regression Engine."""
+import sys
+import warnings
+
 from .core import Dataset, meta_regression
 from .effectsize import OneSampleEffectSizeConverter, TwoSampleEffectSizeConverter
 
@@ -13,3 +16,32 @@ from . import _version
 
 __version__ = _version.get_versions()["version"]
 del _version
+
+
+def _py367_deprecation_warning():
+    """Deprecation warnings message.
+    Notes
+    -----
+    Adapted from NiMARE.
+    """
+    py36_warning = (
+        "Python 3.6 and 3.7 support is deprecated and will be removed in release 0.0.5 of PyMARE. "
+        "Consider switching to Python 3.8, 3.9."
+    )
+    warnings.filterwarnings("once", message=py36_warning)
+    warnings.warn(message=py36_warning, category=FutureWarning, stacklevel=3)
+
+
+def _python_deprecation_warnings():
+    """Raise deprecation warnings.
+    Notes
+    -----
+    Adapted from NiMARE.
+    """
+    if sys.version_info.major == 3 and (
+        sys.version_info.minor == 6 or sys.version_info.minor == 7
+    ):
+        _py367_deprecation_warning()
+
+
+_python_deprecation_warnings()
